c2d9425b4c295f4415bf8685e64e352a7150868d
tim-oe <tim@outboundengine.com>
2021-03-14 09:26:10 -0500

    verified with install  with lite pi os

--- README.md
+++ README.md
@@ -3,23 +3,31 @@ project to house any tweaks and changes for setting up (SkyWeather2)[https://sho
 
 ## Current setup
 
-- Raspberry Pi 4 Model B Rev 1.4 (cat /proc/device-tree/model)
-- Raspbian GNU/Linux 10 buster (cat /etc/os-release)
+- Raspberry Pi 4 Model B Rev 1.4
+    - 8g memory
+    - cat /proc/device-tree/model
+- Raspbian GNU/Linux 10 buster
+    - [Raspberry Pi OS Lite](https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-01-12/2021-01-11-raspios-buster-armhf-lite.zip)
+    - cat /etc/os-release
 - Python 3.7.3
 - 10.3.27-MariaDB-0+deb10u1
 - 64g sd card
 
 ## utility init script
 
-bin/init.sh contains commands ran to initialize the dependent software
+[init.sh](https://github.com/tim-oe/SkyWeather2/blob/main/src/bin/init.sh) contains commands ran to initialize the dependent software
 
 ## sql init script
 
-sql/init.sql contains sql commands to init mariadb
+[init.sql](https://github.com/tim-oe/SkyWeather2/blob/main/src/sql/init.sql) contains sql commands to init mariadb
+
+## steps
+- install pi os
+- enable [ssh](https://www.raspberrypi.org/documentation/remote-access/ssh/)
+- from [manual](https://www.switchdoc.com/wp-content/uploads/2020/12/SkyWeather2AssemblyAndTestManual1.2.pdf)
 
 ## TODOs
 
-- minimal pi install /w out gui
 - add network storage
 - add seconday local storage, ssd
 - make specific db user for Skyweather schema
--- src/bin/init.sh
+++ src/bin/init.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
+# init script to add system and python deps for SkyWeather2
+# requires that wifi is setup
+# ssh enabled to make things easier
 
 # add dependent libraries 
 sudo apt update
 sudo apt upgrade
-# https://forum.switchdoc.com/thread/1482/cmake
+# https://github.com/switchdoclabs/rtl_433/blob/master/README.md
+# git and pip not packaged with lite
 sudo apt install libtool libusb-1.0-0-dev librtlsdr-dev rtl-sdr build-essential autoconf cmake pkg-config mariadb-server python3-pip git
 
 # setup mariadb
-# TODO the SkyWeather app looks to use root to access the db needs to change
+# TODO the SkyWeather app looks to use root to access the db
 sudo mysql_secure_installation
 
 # i would recommend allowing remote access so you can use better tools like toad to look at db
-sudo bash -c 'sed -i "s/^#\?\.bind-address*\$/bind-address=0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf';
+sudo bash -c 'sed -i "s/^bind-address.*\$/bind-address=0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf'
+sudo systemctl restart mysql
 
-# startup ssh
-# sudo systemctl enable ssh
-# sudo systemctl start ssh
-
-# TODO setup
 # add python libraries
 sudo python3 -m pip install apscheduler remi mysqlclient future i2cdevice
 
-# get SkyWeather driver
+# switchdoclabs software defined radio data reciever
 git clone https://github.com/switchdoclabs/rtl_433.git
 
 cd rtl_433/
 mkdir build
 cd build
 cmake ..
+
+# TODO add switch with unput
+
 make
 sudo make install
 
@@ -38,6 +41,10 @@ git clone https://github.com/switchdoclabs/SDL_Pi_SkyWeather2.git
 # load sql
 sudo mysql -u root -p < WeatherSenseWireless.sql
 
-# start config
+# start config app
+# use browser to config
 cd SDL_Pi_SkyWeather2
 python3 SkyWeatherConfigure.py
+
+# test sensors
+sudo python3 testWirelessSensors.py
